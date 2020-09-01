f7cf4d483d9005f1b872268adf441573b45641f4
Mauro <mauro.spinosi94@gmail.com>
2020-09-01 17:10:52 +0200

    tentativo con addclass

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var hamburger = $(".header-right i");
+var yes = $("div.hamburger-menu");
+//
+// hamburger.click(
+//
+// function () {
+//   $(".hamburger-menu").fadeIn("slow");
+//  }
+// );
+//
+// var closeMenu = $(".close");
+//
+// closeMenu.click(
+//   function () {
+//     $(".hamburger-menu").fadeOut("slow");
+//   }
+// );
 
 hamburger.click(
-
-function () {
-  $(".hamburger-menu").fadeIn("slow");
- }
-);
-
-var closeMenu = $(".close");
-
-closeMenu.click(
   function () {
-    $(".hamburger-menu").fadeOut("slow");
+   yes.addClass(".active");
   }
 );
